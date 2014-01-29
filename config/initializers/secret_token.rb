eaae15f3cc853bc365d3499aa3d2301c82b07575
Keith Michael Gregg <keithmichaelgregg@Keiths-MacBook-Pro.local>
2014-01-29 23:20:43 +0000

    created a new rails shell

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aria::Application.config.secret_token = '98c8ad44b2671de78788bc68f6c7018dba27d5f3e028de5f89e66080b376a95d739d9f339fc0adcb224155bb506c3b2eb7e334451e648eb4c1c8eb443a7e482c'
